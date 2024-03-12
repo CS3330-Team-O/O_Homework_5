b0fdfcecbfca056569aad5afbbf9644595a2c5f4
Sydney Poulsen <sydpoulsen@yahoo.com>
2024-03-12 15:59:37 -0500

    added comments to Interfeace strategies

--- src/edu/mu/ICookingStrategy.java
+++ src/edu/mu/ICookingStrategy.java
@@ -1,5 +1,8 @@
 package edu.mu;
 
+//this interface creates a strategy method style of cooking the pizza
+//creates a method signature for cooking the pizza and then is implemented in
+//BrickOvenCookingStrategy, ConventionalOvenCookingStrategy, and MicrowaveCookingStrategy
 public interface ICookingStrategy {
 	public boolean cook(AbstractPizza pizza);
 	
--- src/edu/mu/MicrowaveCookingStrategy.java
+++ src/edu/mu/MicrowaveCookingStrategy.java
@@ -1,14 +1,19 @@
 package edu.mu;
 
 public class MicrowaveCookingStrategy implements ICookingStrategy{
-
+	//set value of additional price for cooking as final so it connot be changed
 	private final double additionalPrice = 1.0;
 
 	@Override
+	//This function cooks the pizza based on the microwave cooking style type
 	public boolean cook(AbstractPizza pizza) {
+		//checks to see if cooking strategy is already set,
 		if(pizza.getCookingStrategy() != null){
 			return false;
 		}
+		//if null then set the cooking strategy to microwave
+				//sets the cooking price to the additional price of the cooking strategy
+				//sets the total price of the pizza by adding the total price and the additionalPrice
 		else {
 			pizza.setCookingStrategy(CookingStyleType.MICROWAVE);
 			pizza.setCookingPrice(additionalPrice);
